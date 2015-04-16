e7d8c1bf6ddd8a99c81b54792f0ca0d8d748d269
Kelno <kelnowm@gmail.com>
2015-04-16 11:59:51 +0200

    a

--- app/viewmodels/masks/maskgeneric.js
+++ app/viewmodels/masks/maskgeneric.js
@@ -3,14 +3,20 @@ define(['durandal/app', 'knockout'],
         var mask = ko.observable(0);
         var bitsList = ko.observableArray();
         var computeList = ko.computed(function() {
+            //clear list
             bitsList([]);
+            //check for max
+            if(mask() >= Math.pow(2,32)) {
+                bitsList.push('Mask is too high');
+                return;
+            }
+            //push present bits
             var i;
-            var maskInt = parseInt(mask());
             for(i = 0; i < 32; i++) {
-                if(maskInt & Math.pow(2,i))
+                if(mask() & Math.pow(2,i))
                     bitsList.push('0x' + Math.pow(2,i).toString(16));
             }
-            return mask();
+            return bitsList;
         });
 
         return {
