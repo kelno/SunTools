7bc64d39fe60fd73453435cf2444b291b51bfb07
Kelno <kelnowm@gmail.com>
2015-10-29 20:08:10 +0100

    random fixes

--- external/fireworks/functions.php
+++ external/fireworks/functions.php
@@ -1,14 +1,24 @@
 <?php
 
+
 function getFirstFreeGroupID()
-{	
-	$queryStr = "SELECT max(groupid) FROM game_event_fireworks;";
-	$query = mysql_query($queryStr);
-	if ($data = mysql_fetch_array($query)) {
-		return $data[0] + 1;
-	} else {
-		return 0;
+{
+	require '../../dbconfig.php';
+
+	try {
+	    $handler = new PDO("mysql:host=".$db['world']['host'].";port=".$db['world']['port'].";dbname=".$db['world']['database']['world'], $db['world']['user'], $db['world']['password'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	    $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+	    echo $e->getMessage();
+	    die();
 	}
+
+	$query = $handler->prepare("SELECT max(groupid) FROM game_event_fireworks");
+	$query->execute();
+	if($data = $query->fetch())
+			return $data[0] + 1;
+
+	return 0;
 }
 
 ?>
--- external/fireworks/index.php
+++ external/fireworks/index.php
@@ -8,14 +8,16 @@
 	}
 	</style>
 	<head>
-		<title> Fireworks editor </title>
+		<title> Fireworks editor - Not finished </title>
 		<meta charset="utf-8"/>
 	</head>
 <body>
 <?php
 
+require('../../dbconfig.php');
+
 try {
-    $handler = new PDO("mysql:host=".$db['suntools']['host'].";port=".$db['suntools']['port'].";dbname=".$db['suntools']['database']['suntools'], $db['suntools']['user'], $db['suntools']['password']);
+    $handler = new PDO("mysql:host=".$db['world']['host'].";port=".$db['world']['port'].";dbname=".$db['world']['database']['world'], $db['world']['user'], $db['world']['password']);
     $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo $e->getMessage();
@@ -36,30 +38,49 @@ if (isset($_GET["id"])) {
 	exit(1);
 }
 
-//delete
-
 $query = $handler->prepare("SELECT time, spellorgob, size, posX, posY, ori, morphX, morphY
-                            FROM game_event_fireworks 
+                            FROM game_event_fireworks
                             WHERE groupid = :id ORDER BY time, posX, posY");
 $query->bindValue(':id', $id, PDO::PARAM_INT);
 $query->execute();
+
+function printCell($index, $value)
+{
+	echo "<td>";
+	switch($index)
+	{
+		case 1:
+			echo "<select>
+							<option value=\"todo\">Red</option>
+							<option value=\"todo\">Blue</option>
+							<option value=\"todo\">Green</option>
+						</select>";
+						break;
+		default:
+			echo "<input type=\"number\" name=\"".$index ."\" value=\"". $value . "\"/></input>";
+			break;
+	}
+	echo "</td>";
+}
 
 echo "<table border=1>";
 echo "<tr><td>Time</td><td>Spell</td><td>Size</td><td>PosX</td><td>PosY</td><td>Ori</td><td>MorphX</td><td>MorphY</td></tr>";
 while ($data = $query->fetch()) {
 	echo "<tr><form>";
 	for($i = 0; $i < 8; $i++)
-		echo "<td><input type=\"text\" name=\"".$i ."\" value=\"". $data[$i] . "\"/></input></td>";
-    echo "<td><input type=\"submit\" value=\"create\"/></td>";
+		printCell($i, $data[$i]);
+
+  echo "<td><input type=\"submit\" value=\"create\"/></td>";
 	echo "<td><input type=\"submit\" value=\"delete\"/></td></form>";
 	echo "</tr>";
 }
 //empty line
-echo "<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>"; 
+echo "<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>";
 //new line for new entry
 echo "<tr><form>";
-for($i = 0; $i < 7; $i++)
-	echo "<td><input type=\"text\" name=\"id\" value=\"0\"/></input></td>";
+for($i = 0; $i < 8; $i++)
+  printCell($i, 0);
+
 echo " <td><input type=\"submit\" value=\"create\"/></td></form>";
 echo "</tr>";
 echo "</table>";
